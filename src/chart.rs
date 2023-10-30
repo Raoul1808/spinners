a8bf3ba463d1daa9c809ad1fff409ef1e2ae8594
Raoul1808 <raoulthegeek@gmail.com>
2023-10-30 21:46:37 +0100

    Deserialize Track Info

--- src/chart.rs
+++ src/chart.rs
@@ -35,3 +35,77 @@ pub struct RawSrtbFile {
     pub large_string_values_container: LargeStringValuesContainer,
     pub clip_info_count: i32,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AlbumArtReference {
+    pub bundle: String,
+    pub asset_name: String,
+    #[serde(rename = "m_guid")]
+    pub m_guid: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BackgroundId {
+    pub background_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BackgroundColoring {
+    pub id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Difficulty {
+    pub bundle: String,
+    pub asset_name: String,
+    #[serde(rename = "m_guid")]
+    pub m_guid: String,
+    #[serde(rename = "_active")]
+    pub active: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TrackInfo {
+    pub track_order: i32,
+    pub track_id: i32,
+    pub album_art_reference: AlbumArtReference,
+    pub background_id: BackgroundId,
+    pub background_coloring: BackgroundColoring,
+    pub artist_name: String,
+    pub feat_artists: String,
+    pub title: String,
+    pub subtitle: String,
+    pub track_label: String,
+    pub charter: String,
+    pub description: String,
+    pub title_text_color: Color,
+    pub subtitle_text_color: Color,
+    pub title_offset_y: i32,
+    pub spotify_link: String,
+    pub apple_music_link: String,
+    pub difficulties: Vec<Difficulty>,
+    pub platform_filter: i32,
+    pub track_type: i32,
+    pub editor_function: String,
+    pub allow_custom_leaderboard_creation: bool,
+}
+
+#[derive(Debug)]
+pub struct SrtbFile {
+    pub raw_content: RawSrtbFile,
+    pub track_info: TrackInfo,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
+use chart::RawSrtbFile;
 use serde_json::Error;
 
 mod chart;
 
-pub fn load_srtb_from_str(str: &str) -> Result<chart::RawSrtbFile, Error> {
-    serde_json::from_str(str)
+pub fn load_srtb_from_str(str: &str) -> Result<chart::SrtbFile, Error> {
+    let raw_file: RawSrtbFile = serde_json::from_str(str)?;
+    let track_info = serde_json::from_str(&raw_file.large_string_values_container.values[0].val.as_str())?;
+    Ok(chart::SrtbFile {
+        raw_content: raw_file,
+        track_info
+    })
 }
 
 pub fn add(left: usize, right: usize) -> usize {
