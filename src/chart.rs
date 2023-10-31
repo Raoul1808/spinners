80cf1f0582f4e5c6afd0984a6887f19f8125ec1f
Raoul1808 <raoulthegeek@gmail.com>
2023-10-31 10:42:26 +0100

    Deserialize TrackData

--- src/chart.rs
+++ src/chart.rs
@@ -37,7 +37,7 @@ pub struct RawSrtbFile {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct AlbumArtReference {
+pub struct AssetBundleReference {
     pub bundle: String,
     pub asset_name: String,
     #[serde(rename = "m_guid")]
@@ -67,7 +67,7 @@ pub struct Color {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Difficulty {
+pub struct ActiveAssetBundleReference {
     pub bundle: String,
     pub asset_name: String,
     #[serde(rename = "m_guid")]
@@ -81,7 +81,7 @@ pub struct Difficulty {
 pub struct TrackInfo {
     pub track_order: i32,
     pub track_id: i32,
-    pub album_art_reference: AlbumArtReference,
+    pub album_art_reference: AssetBundleReference,
     pub background_id: BackgroundId,
     pub background_coloring: BackgroundColoring,
     #[serde(default)]
@@ -99,15 +99,92 @@ pub struct TrackInfo {
     #[serde(default)]
     pub apple_music_link: String,
     pub spotify_link: String,
-    pub difficulties: Vec<Difficulty>,
+    pub difficulties: Vec<ActiveAssetBundleReference>,
     pub platform_filter: i32,
     pub track_type: i32,
     pub editor_function: String,
     pub allow_custom_leaderboard_creation: bool,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PreviewLoopBars {
+    pub min: u32,
+    pub max: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TranslationKey {
+    pub key: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FileReference {
+    #[serde(rename = "m_FileID")]
+    pub file_id: i32,
+    #[serde(rename = "m_PathID")]
+    pub path_id: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ClipData {
+    pub clip_index: i32,
+    pub start_bar: i32,
+    pub end_bar: i32,
+    pub transition_in: i32,
+    pub transition_in_value: f32,
+    pub transition_in_offset: f32,
+    pub transition_out: i32,
+    pub transition_out_value: f32,
+    pub transition_out_offset: f32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Note {
+    pub time: f32,
+    pub r#type: u8,
+    pub color_index: u8,
+    pub column: i32,
+    #[serde(rename = "m_size")]
+    pub m_size: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TrackData {
+    pub revision_version: i32,
+    pub compatibility_version: i32,
+    pub difficulty_rating: u8,
+    pub preview_loop_bars: PreviewLoopBars,
+    pub go_beat_offset_from_first_note: f32,
+    pub difficulty_type: i32,
+    pub is_tutorial: bool,
+    pub tutorial_title_translation: TranslationKey,
+    pub clip_info_asset_references: Vec<AssetBundleReference>,
+    pub background_id: BackgroundId,
+    pub background: AssetBundleReference,
+    pub ground_settings_to_use: AssetBundleReference,
+    pub ground_settings_over_time: AssetBundleReference,
+    // TODO: add beter compatibility here
+    // pub spline_path_data: FileReference,
+    // #[serde(rename = "_feverTime")]
+    // pub fever_time: FileReference,
+    // TODO: tutorial objects
+    // TODO: clip data
+    pub notes: Vec<Note>,
+    // TODO: rewind sections
+    pub last_edited_on_date: String,
+}
+
 #[derive(Debug)]
 pub struct SrtbFile {
     pub raw_content: RawSrtbFile,
     pub track_info: TrackInfo,
+    pub easy_diff: TrackData,
+    pub normal_diff: TrackData,
+    pub hard_diff: TrackData,
+    pub expert_diff: TrackData,
+    pub xd_diff: TrackData,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use chart::RawSrtbFile;
+use chart::{RawSrtbFile, TrackData};
 use serde_json::Error;
 
 mod chart;
@@ -6,9 +6,20 @@ mod chart;
 pub fn load_srtb_from_str(str: &str) -> Result<chart::SrtbFile, Error> {
     let raw_file: RawSrtbFile = serde_json::from_str(str)?;
     let track_info = serde_json::from_str(raw_file.large_string_values_container.values[0].val.as_str())?;
+    let mut diff = Vec::<TrackData>::new();
+    for i in 1..=5 {
+        let str = raw_file.large_string_values_container.values[i].val.as_str();
+        let d = serde_json::from_str(str)?;
+        diff.push(d);
+    }
     Ok(chart::SrtbFile {
         raw_content: raw_file,
-        track_info
+        track_info,
+        xd_diff: diff.pop().unwrap(),
+        expert_diff: diff.pop().unwrap(),
+        hard_diff: diff.pop().unwrap(),
+        normal_diff: diff.pop().unwrap(),
+        easy_diff: diff.pop().unwrap(),
     })
 }
 
